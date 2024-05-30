b77bf4968a436c37566a4049c13fb700489bb1cb
GoldenScout <168351378+GoldenScout@users.noreply.github.com>
2024-05-30 19:49:59 +0530

    Add files via upload

---
+++ ADV C56/main.js
@@ -0,0 +1,6 @@
+function colorchange(){
+    document.getElementById('body').style.backgroundColor = "#171f14"
+    document.getElementById('body').style.color = "#daffcc"
+    document.getElementById('button').style.backgroundColor = "#171f14"
+    document.getElementById('button').style.color = "#daffcc"
+}
